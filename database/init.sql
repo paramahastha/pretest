5f817ece6e08bfd3cfa8bb2d549746eea884a4ac
kahastha <paramahastha@gmail.com>
2019-03-26 18:33:39 +0700

    modify initial db

--- database/init.sql
+++ database/init.sql
@@ -30,7 +30,7 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 2179 (class 0 OID 0)
+-- TOC entry 2185 (class 0 OID 0)
 -- Dependencies: 2
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
@@ -47,7 +47,7 @@ CREATE EXTENSION IF NOT EXISTS adminpack WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 2180 (class 0 OID 0)
+-- TOC entry 2186 (class 0 OID 0)
 -- Dependencies: 1
 -- Name: EXTENSION adminpack; Type: COMMENT; Schema: -; Owner: 
 --
@@ -69,6 +69,21 @@ CREATE SEQUENCE public.ai_category
 
 
 ALTER TABLE public.ai_category OWNER TO postgres;
+
+--
+-- TOC entry 194 (class 1259 OID 32801)
+-- Name: ai_category_product; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.ai_category_product
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.ai_category_product OWNER TO postgres;
 
 --
 -- TOC entry 189 (class 1259 OID 24594)
@@ -100,6 +115,21 @@ CREATE SEQUENCE public.ai_product
 
 ALTER TABLE public.ai_product OWNER TO postgres;
 
+--
+-- TOC entry 193 (class 1259 OID 32777)
+-- Name: ai_product_image; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.ai_product_image
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.ai_product_image OWNER TO postgres;
+
 SET default_tablespace = '';
 
 SET default_with_oids = false;
@@ -119,7 +149,7 @@ CREATE TABLE public.category (
 ALTER TABLE public.category OWNER TO postgres;
 
 --
--- TOC entry 191 (class 1259 OID 24605)
+-- TOC entry 195 (class 1259 OID 32819)
 -- Name: category_product; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -132,7 +162,7 @@ CREATE TABLE public.category_product (
 ALTER TABLE public.category_product OWNER TO postgres;
 
 --
--- TOC entry 192 (class 1259 OID 24620)
+-- TOC entry 191 (class 1259 OID 24620)
 -- Name: image; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -162,7 +192,7 @@ CREATE TABLE public.product (
 ALTER TABLE public.product OWNER TO postgres;
 
 --
--- TOC entry 193 (class 1259 OID 24625)
+-- TOC entry 192 (class 1259 OID 24625)
 -- Name: product_image; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -175,89 +205,127 @@ CREATE TABLE public.product_image (
 ALTER TABLE public.product_image OWNER TO postgres;
 
 --
--- TOC entry 2164 (class 0 OID 24585)
+-- TOC entry 2168 (class 0 OID 24585)
 -- Dependencies: 186
 -- Data for Name: category; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.category (id, enable, name) FROM stdin;
-1	t	book
+8	t	book
+9	t	food
+10	t	shirt
 \.
 
 
 --
--- TOC entry 2169 (class 0 OID 24605)
--- Dependencies: 191
+-- TOC entry 2177 (class 0 OID 32819)
+-- Dependencies: 195
 -- Data for Name: category_product; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.category_product (product_id, category_id) FROM stdin;
-1	1
+10	8
+11	8
+12	8
+13	8
+10	9
+15	8
+16	10
 \.
 
 
 --
--- TOC entry 2170 (class 0 OID 24620)
--- Dependencies: 192
+-- TOC entry 2173 (class 0 OID 24620)
+-- Dependencies: 191
 -- Data for Name: image; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.image (id, name, file, enable) FROM stdin;
-1	harry potter	https://images.ctfassets.net/bxd3o8b291gf/7cpPZFFu92oY46WWiiQcc2/909c7290404dfcda472cb330dc84c135/Harry-Potter-in-cupboard-Jim-Kay-RGB-636x800.jpg?w=1100&q=85	t
+3	uniqlo	https://d2h2vnfmmg5sct.cloudfront.net/catalog/product/large_image/409642_sub6.jpg	t
+4	uniq	https://d2h2vnfmmg5sct.cloudfront.net/catalog/product/large_image/409642_su.jpg	t
+5	abc	https://d2h2vnfmmg5sct.cloudfront.net/catalog/product/large_image/409642_s.jpg	t
 \.
 
 
 --
--- TOC entry 2168 (class 0 OID 24596)
+-- TOC entry 2172 (class 0 OID 24596)
 -- Dependencies: 190
 -- Data for Name: product; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.product (id, name, description, enable) FROM stdin;
-1	harry potter	goblet of fire	t
+9	cba	tshirt	t
+10	cba	tshirt	t
+11	cba	tshirt	t
+12	cba	tshirt	t
+13	cba	tshirt	t
+14	cba	tshirt	t
+15	cba	tshirt	t
+16	cba	tshirt	t
 \.
 
 
 --
--- TOC entry 2171 (class 0 OID 24625)
--- Dependencies: 193
+-- TOC entry 2174 (class 0 OID 24625)
+-- Dependencies: 192
 -- Data for Name: product_image; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.product_image (product_id, image_id) FROM stdin;
-1	1
+10	3
+10	4
+15	3
+16	5
 \.
 
 
 --
--- TOC entry 2181 (class 0 OID 0)
+-- TOC entry 2187 (class 0 OID 0)
 -- Dependencies: 187
 -- Name: ai_category; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.ai_category', 1, true);
+SELECT pg_catalog.setval('public.ai_category', 10, true);
 
 
 --
--- TOC entry 2182 (class 0 OID 0)
+-- TOC entry 2188 (class 0 OID 0)
+-- Dependencies: 194
+-- Name: ai_category_product; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.ai_category_product', 1, false);
+
+
+--
+-- TOC entry 2189 (class 0 OID 0)
 -- Dependencies: 189
 -- Name: ai_image; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.ai_image', 1, true);
+SELECT pg_catalog.setval('public.ai_image', 5, true);
 
 
 --
--- TOC entry 2183 (class 0 OID 0)
+-- TOC entry 2190 (class 0 OID 0)
 -- Dependencies: 188
 -- Name: ai_product; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.ai_product', 1, true);
+SELECT pg_catalog.setval('public.ai_product', 16, true);
 
 
 --
--- TOC entry 2030 (class 2606 OID 24604)
+-- TOC entry 2191 (class 0 OID 0)
+-- Dependencies: 193
+-- Name: ai_product_image; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.ai_product_image', 1, true);
+
+
+--
+-- TOC entry 2034 (class 2606 OID 24604)
 -- Name: category category_id_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -266,7 +334,7 @@ ALTER TABLE ONLY public.category
 
 
 --
--- TOC entry 2032 (class 2606 OID 24589)
+-- TOC entry 2036 (class 2606 OID 24589)
 -- Name: category category_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -275,16 +343,16 @@ ALTER TABLE ONLY public.category
 
 
 --
--- TOC entry 2038 (class 2606 OID 24619)
--- Name: category_product category_product_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- TOC entry 2046 (class 2606 OID 40964)
+-- Name: category_product category_product_product_id_category_id_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.category_product
-    ADD CONSTRAINT category_product_pkey PRIMARY KEY (product_id, category_id);
+    ADD CONSTRAINT category_product_product_id_category_id_key UNIQUE (product_id, category_id);
 
 
 --
--- TOC entry 2040 (class 2606 OID 24624)
+-- TOC entry 2042 (class 2606 OID 24624)
 -- Name: image image_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -293,7 +361,7 @@ ALTER TABLE ONLY public.image
 
 
 --
--- TOC entry 2034 (class 2606 OID 24602)
+-- TOC entry 2038 (class 2606 OID 24602)
 -- Name: product product_id_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -302,16 +370,16 @@ ALTER TABLE ONLY public.product
 
 
 --
--- TOC entry 2042 (class 2606 OID 24629)
--- Name: product_image product_image_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- TOC entry 2044 (class 2606 OID 40966)
+-- Name: product_image product_image_product_id_image_id_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.product_image
-    ADD CONSTRAINT product_image_pkey PRIMARY KEY (product_id, image_id);
+    ADD CONSTRAINT product_image_product_id_image_id_key UNIQUE (product_id, image_id);
 
 
 --
--- TOC entry 2036 (class 2606 OID 24600)
+-- TOC entry 2040 (class 2606 OID 24600)
 -- Name: product product_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -320,7 +388,7 @@ ALTER TABLE ONLY public.product
 
 
 --
--- TOC entry 2043 (class 2606 OID 24608)
+-- TOC entry 2049 (class 2606 OID 32822)
 -- Name: category_product category_product_category_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -329,7 +397,7 @@ ALTER TABLE ONLY public.category_product
 
 
 --
--- TOC entry 2044 (class 2606 OID 24613)
+-- TOC entry 2050 (class 2606 OID 32827)
 -- Name: category_product category_product_product_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -338,7 +406,7 @@ ALTER TABLE ONLY public.category_product
 
 
 --
--- TOC entry 2046 (class 2606 OID 24635)
+-- TOC entry 2048 (class 2606 OID 24635)
 -- Name: product_image product_image_image_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -347,7 +415,7 @@ ALTER TABLE ONLY public.product_image
 
 
 --
--- TOC entry 2045 (class 2606 OID 24630)
+-- TOC entry 2047 (class 2606 OID 24630)
 -- Name: product_image product_image_product_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -355,7 +423,7 @@ ALTER TABLE ONLY public.product_image
     ADD CONSTRAINT product_image_product_id_fkey FOREIGN KEY (product_id) REFERENCES public.product(id);
 
 
--- Completed on 2019-03-26 11:02:19 WIB
+-- Completed on 2019-03-26 18:19:38 WIB
 
 --
 -- PostgreSQL database dump complete
